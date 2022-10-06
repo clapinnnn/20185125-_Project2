12a0206769737623f002285fbe843f05aedaa0d2
수인 박 <psi7599@naver.com>
2022-10-06 18:09:27 +0900

    초기 커밋.

---
+++ 20185125박수인_Project2/소스.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	
+	// p.115, 1번 문제
+	string string1, string2;
+
+	cout << "첫 번째 문자열: ";
+	cin >> string1;
+
+	cout << "두 번째 문자열: ";
+	cin >> string2;
+
+	if (string1.compare(string2))
+		cout << "2개의 문자열은 다릅니다." << endl;
+	else
+		cout << "2개의 문자열은 같습니다." << endl;
+	cout << endl;
+
+	
+	// p.117, 10번 문제
+	for (int a = 1; a <= 100; a++) 
+		for (int b = 1; b <= 100; b++) 
+			for (int c = 1; c <= 100; c++) 
+				if (a * a + b * b == c * c)
+					cout << a << " " << b << " " << c << endl;
+	cout << endl;
+
+	
+	// p.119, 16번 문제
+	int cash, bets, win;
+	bool result;
+	int total = 0;
+	int wins = 0;
+
+	srand(time(NULL));
+
+	cout << "초기 금액 $50" << endl;
+	cout << "목표 금액 $250" << endl;
+
+	for (int game = 0; game < 1000; game++) {
+		cash = 50;
+		bets = 0;
+		win = 0;
+
+		while (true) {
+			bets++;
+			if ((double)rand() / RAND_MAX < 0.5) {
+				cash++;
+				win++;
+			}
+			else cash--;
+
+			if (cash <= 0) {
+				result = false;
+				break;
+			}
+			if (cash >= 250) {
+				result = true;
+				break;
+			}
+		}
+		if (result == true)
+			wins++;
+		total += bets;
+	}
+
+	cout << "1000 중의 " << wins << "번 승리" << endl;
+	cout << fixed;
+	cout.precision(6);
+	cout << "이긴 확률=" << wins / 1000.0 * 100.0 << endl;
+	cout << "평균 배팅 횟수 = " << total / 1000.0 << endl;
+
+}
---
+++ Ex_02/소스.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+inline double cal_volume(double radius = 5) {	//default 인수 기능
+	double v = (4.0 / 3.0 * 3.14 * pow(radius, 3));
+	return v;
+}
+
+int main() {
+	
+	// p.160, 2번 문제
+	double radius;
+
+	cout << "반지름을 입력하시오: ";
+	cin >> radius;
+
+	cout << "구의 부피는 " << cal_volume(radius) << endl;
+	cout << endl;
+	
+
+	// p.162, 9번 문제
+	srand(time(NULL));
+
+	int dice1 = rand() % 6 + 1;
+	int dice2 = rand() % 6 + 1;
+	int result = dice1 + dice2;
+
+	cout << "사용자의 주사위: " << dice1 << "+" << dice2 << "=" << result << endl;
+
+	if (result == 7 || result == 11)
+		cout << "사용자가 이겼습니다." << endl;
+	else if (result == 2 || result == 3 || result == 12)
+		cout << "사용자가 졌습니다." << endl;
+	else
+		cout << "사용자가 비겼습니다." << endl;
+	cout << endl;
+	
+
+	// p.164, 13번 문제
+	string pw;
+	bool upper = false, lower = false, digit = false;
+
+	cout << "암호를 입력하시오: ";
+	cin >> pw;
+
+	for (auto& c : pw) {
+		if (isupper(c))
+			upper = true;
+		if (islower(c))
+			lower = true;
+		if (isdigit(c))
+			digit = true;
+	}
+
+	if (upper && lower && digit)
+		cout << "안전합니다." << endl;
+	else
+		cout << "안전하지 않습니다." << endl;
+		
+	
+
+}
